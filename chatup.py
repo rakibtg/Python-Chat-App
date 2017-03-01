8b92a95e1bacad04ab9b33bcdc082c6425ab8a6a
Kazi Mehedi Hasan <rakibtg@gmail.com>
2017-03-02 00:47:53 +0600

    completed basics

---
+++ chatup.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+# https://flask-socketio.readthedocs.io/en/latest/
+# https://github.com/socketio/socket.io-client
+
+app = Flask(__name__)
+
+app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
+socketio = SocketIO( app )
+
+@app.route( '/' )
+def hello():
+  return render_template( './ChatApp.html' )
+
+def messageRecived():
+  print( 'message was received!!!' )
+
+@socketio.on( 'my event' )
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) )
+  socketio.emit( 'my response', json, callback=messageRecived )
+
+if __name__ == '__main__':
+  socketio.run( app, debug = True )
